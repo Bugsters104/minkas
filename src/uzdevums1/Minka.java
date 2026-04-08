feecb73a25ccd334b231ef76603fd392bc699f1c
Bugsters104 <user@pasts.com>
2026-04-08 21:52:33 +0300

    12. nodarbības beigas

--- src/uzdevums1/Minka.java
+++ src/uzdevums1/Minka.java
@@ -1,5 +1,40 @@
 package uzdevums1;
 
-public class Minka {
+import java.time.Year;
+import java.util.Random;
 
+import javax.swing.ImageIcon;
+
+public class Minka {
+	// Atribūti
+	String vards, skirne, spalvasKrasa, saimnieks;
+	int vecums, medijumuSk, izsalkums;
+	boolean siksnina;
+	ImageIcon bilde;
+	Random rand = new Random();
+	
+	// Konstruktors
+	public Minka(String vards, String skirne, String spalvasKrasa,
+		String saimnieks, int dzGads, boolean siksnina, String cels) {
+		this.vards = vards;
+		this.skirne = skirne;
+		this.spalvasKrasa = spalvasKrasa;
+		this.saimnieks = saimnieks;
+		vecums = Year.now().getValue() - dzGads;
+		medijumuSk = 0;
+		izsalkums = kungis();
+		this.siksnina = siksnina;
+		bilde = iestatitBildi(cels);
+	}
+	
+	// Metodes
+	public int kungis() {
+		return rand.nextInt(5)+1;
+	}
+	
+	public ImageIcon iestatitBildi(String cels) {
+		// Turpināsim nākošajā nodarbībā
+		
+		return null;
+	}
 }
