e8360834267acc7c2b2b558bd9f1a379c810fa54
Bugsters104 <user@pasts.com>
2026-04-13 19:44:30 +0300

    Pabeigta klase 'Minka'

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ![Minka foto](https://static.vecteezy.com/system/resources/thumbnails/053/317/080/small/sweet-little-cat-sitting-on-transparent-background-free-png.png)
 
 **Darāmo darbu saraksts:**
-- [ ] Izveidot klasi "Minka"
+- [x] Izveidot klasi "Minka"
 - [ ] Izveidot klasei "MinkuTante"
 - [ ] Izveidot klasi "DarbsArFailu"
 - [ ] Versionēt visu izstrādes procesu
--- src/uzdevums1/Minka.java
+++ src/uzdevums1/Minka.java
@@ -1,9 +1,12 @@
 package uzdevums1;
 
+import java.awt.Image;
+import java.net.URL;
 import java.time.Year;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class Minka {
 	// Atribūti
@@ -33,8 +36,102 @@ public class Minka {
 	}
 	
 	public ImageIcon iestatitBildi(String cels) {
-		// Turpināsim nākošajā nodarbībā
+		if(!cels.endsWith(".png"))
+			cels += ".png";
 		
-		return null;
+		URL resurss = getClass().getResource("/atteli/"+ cels);
+		if(resurss != null)
+			return new ImageIcon(new ImageIcon(resurss).getImage().
+					getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Nevar atrast bildi: "+cels, 
+					"Brīdinājums", JOptionPane.WARNING_MESSAGE);
+			return null;
+		}
+	}
+	
+	void murrat() {
+		JOptionPane.showMessageDialog(null, 
+		vards + "saka 'murr murr murrr!'", "Paziņojums", 
+		JOptionPane.PLAIN_MESSAGE, bilde);
+	}
+	
+	String nolasitAtributus() {
+		return vards + " ir " + vecums + " gadus vecs " + skirne + 
+				" šķirnes kaķis ar " + spalvasKrasa + " krāsas kažoku."
+		+ "\nKaķim " + ((siksnina) ? "ir" : "nav") + " siksniņa."
+		+ "\nKaķa saimnieks ir "+ saimnieks + 
+		".\nMedījumu skaits: "+ medijumuSk;
+	}
+	
+	String pabarot(String ediens) {
+		if(izsalkums > 0) {
+			bilde = iestatitBildi("trauks");
+			JOptionPane.showMessageDialog(null, "Ņamm ņamm, man garšo "+
+			ediens, "Informācija", JOptionPane.INFORMATION_MESSAGE, bilde);
+			ediens = "Tukša bļoda";
+			izsalkums--;
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "Murr.., esmu pārēdies un "
+			+ ediens + " nevēlos!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+			izsalkums = kungis();
+		}
+		return ediens;
+	}
+	
+	// Metožu pārslogošana
+	void gule() {
+		JOptionPane.showMessageDialog(null, saimnieks + 
+		"! Aizmirsi iedot man dzesiņu, nevarēšu pagulēt!", "Kļūda", 
+		JOptionPane.ERROR_MESSAGE);
+	}
+	
+	void gulet(String prieksmets) {
+		if(prieksmets.equalsIgnoreCase("sedziņa")) {
+			bilde = iestatitBildi("gul");
+			JOptionPane.showMessageDialog(null, "Zzzz....", "Informācija", 
+			JOptionPane.INFORMATION_MESSAGE, bilde);
+		
+		} else
+			JOptionPane.showMessageDialog(null, saimnieks + "! "
+		+ prieksmets + " man neder gulēšanai! Šššss ņau!", "Kļūda", 
+		JOptionPane.ERROR_MESSAGE);
+	}
+	
+	void palielinatVecumu() {
+		if(vecums < 20) {
+			vecums++;
+			bilde = iestatitBildi("kuka");
+			JOptionPane.showMessageDialog(null, vards + 
+			" vecums palielināts par vienu gadu!", "Informācija", 
+			JOptionPane.INFORMATION_MESSAGE, bilde);
+		
+		} else 
+			JOptionPane.showMessageDialog(null,
+			"Sasniegts maksimālais vecums!", "Brīdinājums", 
+			JOptionPane.WARNING_MESSAGE);
+	}
+	
+	void nolasitVecumu() {
+		JOptionPane.showMessageDialog(null, vards + " ir " + 
+	vecums + " gadus vecs",
+	"Informācija", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	void medit() {
+		if((rand.nextInt(3)) == 0) {
+			medijumuSk++;
+			bilde = iestatitBildi("nokerts");
+			JOptionPane.showMessageDialog(null,
+			"Medījums tika notverts!", "Veiksme", 
+			JOptionPane.INFORMATION_MESSAGE, bilde);
+		
+		} else { 
+			bilde = iestatitBildi("aizlaidas");
+			JOptionPane.showMessageDialog(null, "Medījums aizlaidās!", 
+			"Neveiksme", JOptionPane.INFORMATION_MESSAGE, bilde);
+		}
 	}
 }
